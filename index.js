551c712f76c911f2888ca6d727633c5280f7e377
WILL IRVIN <william_irvin@live.com>
2017-09-28 21:56:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ if (name === "Susan") {
   console.log ("The name is correct!");
 }
 else {
-  console.log(name = "Expected: ,") (name = "Received: ")
+  console.log(name = "Expected: , Received: ")
 }
 
 if (height === "70") {
