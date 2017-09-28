11cebbd0f1d43f2c4aa378b508e20048b12b3d87
WILL IRVIN <william_irvin@live.com>
2017-09-28 21:12:49 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,13 +1,17 @@
+var name = "Joe"
+var height = "70"
+
+
 if (name === "Joe") {
   console.log("The name is correct")
-} 
+}
 else {
   console.log ("Expected the name to be Susan")
 }
 
 if (height === "70") {
   console.log("The height is correct")
-} 
+}
 else {
   console.log("Expected the height to be 70")
 }
