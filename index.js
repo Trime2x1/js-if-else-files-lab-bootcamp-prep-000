e34d8621914c75e7200d5288f75ed79c10e1c6de
WILL IRVIN <william_irvin@live.com>
2017-09-28 21:21:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var name = "Susan"
+var name = "Joe"
 var height = "74"
 
 if (name === "Susan") {
   console.log ("The name is correct");
 }
 else {
-  console.log("Expected the name to be Susan")
+  console.log("Expected: index.name," "Received: index.name")
 }
 
 // Don't worry about this
